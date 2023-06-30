36bc92afb60bba4beb6f0f2bea512a28240e7637
2006040110 <131932612+2006040110@users.noreply.github.com>
2023-06-30 10:16:21 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -40,22 +40,25 @@ def read_data():
 class NeuralNetwork(nn.Module):
     def __init__(self, num_classes=10):
         super(NeuralNetwork, self).__init__()
+
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(3, 64, kernel_size=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=2)
         )
+
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 320),
@@ -63,13 +66,13 @@ class NeuralNetwork(nn.Module):
             nn.Dropout(),
             nn.Linear(320, 256),
             nn.ReLU(inplace=True),
-            nn.Linear(256, num_classes),
+            nn.Linear(256, num_classes)
         )
 
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
 model = NeuralNetwork().to(device)
